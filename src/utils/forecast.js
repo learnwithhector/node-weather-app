937e61ad74b6f1494c6bfa2b94c034dc112a8436
Hector Macafonty <learnwithhector@gmail.com>
2023-01-30 23:49:40 +0000

    node weather app

---
+++ src/utils/forecast.js
@@ -0,0 +1,24 @@
+const request = require("request");
+
+const forecast = (latitude, longitude, callback) => {
+  const url = `http://api.weatherstack.com/current?access_key=c379acfde0aa5e7837dbe1eca7389d12&query=${latitude},${longitude}&units=m`;
+
+  request({ url, json: true }, (error, { body } = {}) => {
+    if (error) {
+      callback("Unable to connect to weather services", undefined);
+    } else if (body.error) {
+      callback("Unable to find location", undefined);
+    } else {
+      const temperature = body.current.temperature;
+      const feelsLike = body.current.feelslike;
+      const forecast = body.current.weather_descriptions[0];
+
+      callback(
+        undefined,
+        `${forecast}. The temperature is ${temperature} degrees. It feels like ${feelsLike} degrees`
+      );
+    }
+  });
+};
+
+module.exports = forecast;

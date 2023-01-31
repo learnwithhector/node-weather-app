8221848c0d52e62c2fc451294ef0adf4a4991ce3
Hector Macafonty <learnwithhector@gmail.com>
2023-01-31 01:57:25 +0000

    added uv index to weather forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -12,10 +12,11 @@ const forecast = (latitude, longitude, callback) => {
       const temperature = body.current.temperature;
       const feelsLike = body.current.feelslike;
       const forecast = body.current.weather_descriptions[0];
+      const uv = body.current.uv_index;
 
       callback(
         undefined,
-        `${forecast}. The temperature is ${temperature} degrees. It feels like ${feelsLike} degrees`
+        `${forecast}. The temperature is ${temperature} degrees. It feels like ${feelsLike} degrees. The uv index is ${uv}.`
       );
     }
   });
